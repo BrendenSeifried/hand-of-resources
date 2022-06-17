d716ccd1d447d433f1dd200270ea9cc463131e80
BrendenSeifried <brendenseifried@gmail.com>
2022-06-17 15:29:15 -0700

    test passing for rendering list

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -5,10 +5,14 @@ const app = require('../lib/app');
 
 describe('Games Table Tests', () => {
   it('Test to confirm a list of games are rendered', async () => {
-    const resp = await require(app).get('/games');
+    const resp = await request(app).get('/games');
     expect(resp.status).toEqual(200);
-    expect(resp.body.length).toEqual(7);
-    const select = resp.body.find((item) => item.name === 'Road Rash64');
-    expect(select).toHaveProperty('name', 'Road Rash64');
+    expect(resp.body.length).toEqual(5);
+    const select = resp.body.find((item) => item.title === 'Road Rash64');
+    expect(select).toHaveProperty('title', 'Road Rash64');
+  });
+
+  afterAll(() => {
+    pool.end();
   });
 });

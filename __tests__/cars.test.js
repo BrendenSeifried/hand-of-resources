e118b2d0e93bad23f2bccb3c1adce8a9649b5544
BrendenSeifried <brendenseifried@gmail.com>
2022-06-17 20:29:37 -0700

    CAR model created test refactored to look for model of car instead of make tests passing

--- __tests__/cars.test.js
+++ __tests__/cars.test.js
@@ -9,10 +9,14 @@ describe('Cars table tests', () => {
   });
 
   it('Test to render list of cars (4)', async () => {
-    const resp = await request(app).get('/nbaTeams');
-    expect(resp.body).toEqual(200);
+    const resp = await request(app).get('/cars');
+    expect(resp.status).toEqual(200);
     expect(resp.body.length).toEqual(4);
-    const one = resp.body.find((item) => item.make === 'Honda');
-    expect(one).toHaveProperty('make', 'Honda');
+    const one = resp.body.find((item) => item.model === 'Civic');
+    expect(one).toHaveProperty('model', 'Civic');
+  });
+
+  afterAll(() => {
+    pool.end();
   });
 });
